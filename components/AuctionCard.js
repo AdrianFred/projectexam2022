459723e6c3060f7d46188b6d01360f62a09d28b0
AdrianFred <78622520+AdrianFred@users.noreply.github.com>
2022-12-06 21:31:23 +0100

    Changing api fetch on cards

--- components/AuctionCard.js
+++ components/AuctionCard.js
@@ -44,7 +44,7 @@ export default function AuctionCard({ auctions }) {
   const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch("https://api.noroff.dev/api/v1/auction/listings", {
+      const res = await fetch("https://api.noroff.dev/api/v1/auction/listings?limit=10", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -52,33 +52,34 @@ export default function AuctionCard({ auctions }) {
         body: undefined,
       });
       const data = await res.json();
-      console.log(data);
-      //   setData(data);
-      if (data) {
-        setData(data);
-      }
+      setData(data);
     }
     fetchData();
   }, []);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-4">
-        {data.map((auctions) => (
-          <div className="flex shadow-2xl  rounded-tr-3xl rounded-bl-3xl bg-white p-4 min-w-[250px]">
-            <div>
-              <div className="pt-4 text-center">Time Remaining</div>
-              <div className="text-green font-bold pt-2 text-center">{auctions.endsAt}</div>
-              <div>
-                <img className="pt-3 rounded-tr-3xl rounded-bl-3xl" src={auctions.media} alt="/" width={250} height={150} />
+        {data.map((auctions) => {
+          if (auctions.media.length !== 0) {
+            return (
+              <div key={auctions.id} className="flex shadow-2xl  rounded-tr-3xl rounded-bl-3xl bg-white p-4 min-w-[250px]">
+                <div>
+                  <div className="pt-4 text-center">Time Remaining</div>
+                  <div className="text-green font-bold pt-2 text-center">{auctions.endsAt}</div>
+                  <div>
+                    <img className="pt-3 rounded-tr-3xl rounded-bl-3xl" src={auctions.media} alt="/" width={250} height={150} />
+                  </div>
+                  <div className="font-bold ml-6 mt-4">{auctions.title}</div>
+                  <div className="ml-6 mt-2">{auctions.bids}</div>
+                  <div className="">
+                    <button className="bg-green p-2 ml-4 mt-8 rounded-3xl text-white min-w-[150px]">Check Item</button>
+                  </div>
+                </div>
               </div>
-              <div className="font-bold ml-6 mt-4">{auctions.title}</div>
-              <div className="ml-6 mt-2">{auctions.bids}</div>
-              <div className="">
-                <button className="bg-green p-2 ml-4 mt-8 rounded-3xl text-white min-w-[150px]">Check Item</button>
-              </div>
-            </div>
-          </div>
-        ))}
+            );
+          }
+        })}
       </div>
     </>
   );
