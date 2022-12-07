1909161475773dd7021046345f4cb9b31439017a
AdrianFred <78622520+AdrianFred@users.noreply.github.com>
2022-12-07 23:14:24 +0100

    useEffect if user is logged in or not

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { useRouter } from "next/router";
 
 const Navbar = () => {
@@ -12,17 +10,14 @@ const Navbar = () => {
   const [shadow, setShadow] = useState(false);
   const [navBg, setNavBg] = useState("#ecf0f3");
   const [navColor, setNavColor] = useState("#1f2937");
+  const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (router.pathname === "/property") {
-      setNavBg("transparent");
-      setNavColor("#ecf0f3");
-    } else {
-      setNavBg("#ecf0f3");
-      setNavColor("#1f2937");
+    if (localStorage.getItem("token")) {
+      setLoggedIn(true);
     }
-  }, [router]);
+  }, []);
 
   const toggleNav = () => {
     setNav(!nav);
@@ -39,8 +34,14 @@ const Navbar = () => {
     window.addEventListener("scroll", handleShadow);
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("credits");
+    router.reload();
+  };
+
   return (
-    <div style={{ backgroundColor: `${navBg}` }} className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
+    <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
       <div className="flex justify-between items-center w-full h-full px-8 2xl:px-16 ">
         <Link href="/">
           <Image src="/../public/assets/Logo.png" alt="/" width={100} height={100} />
@@ -72,7 +73,7 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? "fixed left-0 top-0 w-[100%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              ? "fixed left-0 top-0 w-[100%] sm:w-[70%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
               : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
           }
         >
@@ -115,12 +116,24 @@ const Navbar = () => {
             <div className="pt-10">
               <p className="uppercase tracking-widest text-green-500 ">Logged in as</p>
               <div className="flex items-center justify-around my-4 w-full sm:w-[80%]">
-                <Link href="/login">
-                  <button className="p-3 bg-green rounded-xl">Log in</button>
-                </Link>
-                <Link href="/register">
-                  <button className="p-3 bg-green rounded-xl">Sign up</button>
-                </Link>
+                {!loggedIn ? (
+                  <Link href="/login">
+                    <button className="py-4 px-8 bg-green rounded-xl text-white">Log in</button>
+                  </Link>
+                ) : (
+                  <Link href="/profile">
+                    <button className="py-4 px-8 bg-green rounded-xl text-white">Profile</button>
+                  </Link>
+                )}
+                {!loggedIn ? (
+                  <Link href="/register">
+                    <button className="py-4 px-8 bg-green rounded-xl text-white">Sign up</button>
+                  </Link>
+                ) : (
+                  <button onClick={handleLogout} className="py-4 px-8 bg-red-500 rounded-xl text-white">
+                    Log out
+                  </button>
+                )}
               </div>
             </div>
           </div>
