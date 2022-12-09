7f45b78cf635e85d4ae03fe55dd32adba4f4a6ee
AdrianFred <adrian989@me.com>
2022-12-09 01:09:34 +0100

    Making profile react with the api

--- pages/profile.js
+++ pages/profile.js
@@ -1,30 +1,55 @@
-import propsImg from "../public/assets/man.jpg";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+  const [userInfo, setUserInfo] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://api.noroff.dev/api/v1/auction/profiles/${localStorage.getItem("name")}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: undefined,
+      });
+      const data = await res.json();
+      setUserInfo(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="debug-screens">
       <div className="flex justify-center">
         <div>
           <div className="text-2xl text-green">Profile</div>
           <div className="pt-6">
-            <img src="/assets/man.jpg" className="w-[300px] rounded-2xl"></img>
+            {!userInfo.image ? (
+              <img src="/assets/man.jpg" className="w-[300px] rounded-2xl"></img>
+            ) : (
+              <img src={userInfo.image} className="w-[300px] rounded-2xl"></img>
+            )}
+
             <div className="flex justify-center pt-6">
               <button className="bg-green p-3 rounded-3xl text-white ">Change profile picture?</button>
             </div>
           </div>
-          <div className="text-xl pt-14">
+          <div className="text-lg pt-8">
             <div>
               <p className="text-green">Name</p>
-              <p className="ml-4">Username</p>
+              <p className="ml-4">{userInfo.name}</p>
             </div>
             <div>
               <p className="text-green">Email</p>
-              <p className="ml-4">email.noroff@test.no</p>
+              <p className="ml-4">{userInfo.email}</p>
             </div>
             <div>
               <p className="text-green">Credits</p>
               <p className="ml-4">
-                <span className="font-bold">1000</span> Credits available
+                <span className="font-bold">{userInfo.credits}</span> Credits available
               </p>
             </div>
           </div>
