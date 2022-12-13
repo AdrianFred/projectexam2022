0039a951053f0357655b5974c3081ed140bbf46a
AdrianFred <adrian989@me.com>
2022-12-13 15:43:09 +0100

    Making it possible to add multiple media's

--- components/NewListings.jsx
+++ components/NewListings.jsx
@@ -1,25 +1,27 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import { convertDate } from "./tools/DateFormatter";
+import { HiPlusSm } from "react-icons/hi";
 
 export default function NewListings() {
   const router = useRouter();
   const [input, setInput] = useState({
     title: "",
     description: "",
-    media: [],
-    endsAt: "2022-12-14T21:44:58.853Z",
+    media: [""],
+    endsAt: "",
   });
-  const changeInput = (e) => {
-    setInput({ ...input, [e.target.name]: e.target.value });
-    console.log(input);
-  };
-  const changeMedia = (e) => {
+
+  const onChangers = (e) => {
     if (e.target.name === "media") {
-      setInput({ ...input, media: [e.target.value] });
+      setInput({ ...input, media: [...input.media, e.target.value] });
+    } else if (e.target.name === "endsAt") {
+      let dates = convertDate(e.target.value);
+      setInput({ ...input, endsAt: dates });
+    } else if (e.target.name === "title" || e.target.name === "description") {
+      setInput({ ...input, [e.target.name]: e.target.value });
     }
-    console.log(input);
   };
 
   const handleSubmit = async (e) => {
@@ -38,7 +40,8 @@ export default function NewListings() {
       toast.success("Listing created");
       router.push(`/auction/${data.id}`);
     } else {
-      toast.error("Something went wrong");
+      const errors = data.errors;
+      errors.forEach((error) => toast.error(error.message));
     }
   };
 
@@ -46,14 +49,14 @@ export default function NewListings() {
     <>
       <div className="debug-screens flex justify-center items-center">
         <div className="">
-          <div className="bg-white h-[500px] w-[350px] rounded-xl shadow-xl p-6">
+          <div className="bg-white h-auto w-[350px] rounded-xl shadow-xl p-6">
             <div className="text-green text-3xl">Make New Listing</div>
             <div className="flex justify-center items-center h-[450px]">
               <form>
                 <div className="flex flex-col mt-4 min-w-[250px]">
                   <label className="text-sm font-bold">Post-Title</label>
                   <input
-                    onChange={changeInput}
+                    onChange={onChangers}
                     placeholder="A nice car"
                     name="title"
                     type="text"
@@ -66,7 +69,7 @@ export default function NewListings() {
                 <div className="flex flex-col mt-4 min-w-[250px]">
                   <label className="text-sm font-bold">Description</label>
                   <textarea
-                    onChange={changeInput}
+                    onChange={onChangers}
                     name="description"
                     type="textarea"
                     className="border-b border-gray-300 focus:outline-none focus:border-green"
@@ -77,21 +80,32 @@ export default function NewListings() {
                 </div>
                 <div className="flex flex-col mt-4 min-w-[250px]">
                   <label className="text-sm font-bold">Media/URL</label>
-                  <input
-                    onChange={changeMedia}
-                    name="media"
-                    type="text"
-                    className="border-b border-gray-300 focus:outline-none focus:border-green"
-                    required
-                    minLength={8}
-                    pattern="https?://.+"
-                    Title="Please enter a valid URL"
-                  />
+                  <div id="mediaInput">
+                    {input.media.map((media, index) => (
+                      <input
+                        className="border-b border-gray-300 focus:outline-none focus:border-green w-full"
+                        key={index}
+                        type="text"
+                        value={media}
+                        required
+                        pattern="https?://.+"
+                        onChange={(e) =>
+                          setInput({
+                            ...input,
+                            media: input.media.map((m, i) => (i === index ? e.target.value : m)),
+                          })
+                        }
+                      />
+                    ))}
+                    <button onClick={() => setInput({ ...input, media: [...input.media, ""] })}>
+                      <HiPlusSm />
+                    </button>
+                  </div>
                 </div>
                 <div className="flex flex-col mt-4 min-w-[250px]">
                   <label className="text-sm font-bold">Ending Date</label>
                   <input
-                    // onChange={changeInput}
+                    onChange={onChangers}
                     name="endsAt"
                     type="date"
                     className="border-b border-gray-300 focus:outline-none focus:border-green"
