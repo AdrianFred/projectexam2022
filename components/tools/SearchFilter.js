958029ef0fd371787fa11fbf23878aae11fa4347
AdrianFred <adrian989@me.com>
2022-12-16 18:45:41 +0100

    Making an exportable filter result function

---
+++ components/tools/SearchFilter.js
@@ -0,0 +1,19 @@
+export function filterResults(searchTerm, { results }) {
+  const filteredResults = [];
+  results.map((result) => {
+    if (result.description === null) {
+      if (result.title.toLowerCase().includes(searchTerm.toLowerCase()) || result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        filteredResults.push(result);
+      }
+    } else {
+      if (
+        result.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        result.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        filteredResults.push(result);
+      }
+    }
+  });
+  return filteredResults;
+}
