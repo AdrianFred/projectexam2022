98147178c43bb174a021a4a354ff7fe5662a2428
AdrianFred <adrian989@me.com>
2022-12-17 01:34:55 +0100

    Importing poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: { ...colors, green: "#0C5F3B", bg: "#f2f2f2", black: "#08090a" },
+      colors: { ...colors, green: "#cd2026", bg: "#f2f2f2", black: "#08090a" },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [require("tailwindcss-debug-screens"), require("@tailwindcss/line-clamp")],
 };
+
+// 0C5F3B
