b7f588e6c2e34756546cbb1e3d238d22466ff2af
AdrianFred <adrian989@me.com>
2022-12-17 01:37:27 +0100

    Edit search filter to only include if theres media

--- components/tools/SearchFilter.js
+++ components/tools/SearchFilter.js
@@ -1,17 +1,19 @@
 export function filterResults(searchTerm, { results }) {
   const filteredResults = [];
   results.map((result) => {
-    if (result.description === null) {
-      if (result.title.toLowerCase().includes(searchTerm.toLowerCase()) || result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-        filteredResults.push(result);
-      }
-    } else {
-      if (
-        result.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())
-      ) {
-        filteredResults.push(result);
+    if (result.media.length !== 0) {
+      if (result.description === null) {
+        if (result.title.toLowerCase().includes(searchTerm.toLowerCase()) || result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          filteredResults.push(result);
+        }
+      } else {
+        if (
+          result.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          result.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          result.seller.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          filteredResults.push(result);
+        }
       }
     }
   });
