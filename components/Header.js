3202b3ccc57effe0eac95582d1ca9b75deff5472
AdrianFred <adrian989@me.com>
2022-12-18 14:22:41 +0100

    outline red

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="debug-screens ">
       <div className="bg-black min-h-[250px] min-w-full flex justify-center items-center">
         <div className="">
-          <h2 className="text-white text-base text-center">Welcome to the Auction House</h2>
+          <h2 className="text-white text-base font-bold outline outline-red text-center">Welcome to the Auction House</h2>
           <h2 className="text-white font-bold text-2xl text-center pt-3">Buy and sell your goods</h2>
           <p className="text-white text-sm max-w-[200px] text-center mx-auto pt-3">
             Auction House is expanding to the web to supply both online and locally
